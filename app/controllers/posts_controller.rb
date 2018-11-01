05190c70c465b17f76a0e628a38179acd0414171
Your Name <ec2-user@ip-172-31-12-1.us-east-2.compute.internal>
2018-11-01 15:04:00 +0000

    initializing app

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,42 @@
-class ArticlesController < ApplicationController 
+class PostsController < ApplicationController 
+    def index
+        @posts = Post.all
+    end
+    def show
+        @post = Post.find(params[:id])
+    end
     def new
     end
 
     def create
+        @post = Post.new(post_params)
         
+        @post.save
+        redirect_to @post
     end
+    def edit
+        @post = Post.find(params[:id])
+    end
+    def update
+        @post = Post.find(params[:id])
+        
+        if @post.update(post_params)
+            redirect_to @post
+        else
+            render 'edit'
+        end
+    end
+    def destroy
+        @post = Post.find(params[:id])
+        post.destroy
+        
+        redirect_to posts_path
+    end
+    
 end
+
+private
+    def post_params
+        params.require(:post).permit(:title, :text)
+    end
+    
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_23_153748) do
+ActiveRecord::Schema.define(version: 2018_10_23_170821) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
@@ -18,5 +18,6 @@ ActiveRecord::Schema.define(version: 2018_10_23_153748) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
 
 end
