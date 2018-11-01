05208f809cc0d77b2b354680d78c8bcfe51e39a9
Your Name <ec2-user@ip-172-31-12-1.us-east-2.compute.internal>
2018-11-01 17:51:48 +0000

    Made the reply controller and what not

---
+++ app/controllers/replies_controller.rb
@@ -0,0 +1,12 @@
+class RepliesController < ApplicationController
+    def create
+        @post = Post.find(params[:post_id])
+        @reply = @post.replies.create(reply_params)
+        redirect_to post_path(@post)
+    end
+    
+    private
+        def reply_params
+            params.require(:reply).permit(:commenter, :body)
+        end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_01_152730) do
+ActiveRecord::Schema.define(version: 2018_11_01_162148) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "text"
+    t.string "category"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
