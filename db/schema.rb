582a4e372ea7ae68e33e81be0eca521b5abc6bb7
Brian Stone <bstone26@uncc.edu>
2018-11-29 15:33:16 +0000

    Add users model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_01_162148) do
+ActiveRecord::Schema.define(version: 2018_11_29_153211) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
@@ -27,6 +27,16 @@ ActiveRecord::Schema.define(version: 2018_11_01_162148) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["post_id"], name: "index_replies_on_post_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "provider"
+    t.string "uid"
+    t.string "name"
+    t.string "oauth_token"
+    t.datetime "oauth_expires_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
