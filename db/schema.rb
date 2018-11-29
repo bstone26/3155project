22577464b0f284c13b6556205bbdfcd6de4b5b37
Brian Stone <bstone26@uncc.edu>
2018-11-29 18:56:02 +0000

    Add major attrigbut to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_29_183933) do
+ActiveRecord::Schema.define(version: 2018_11_29_185531) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 2018_11_29_183933) do
     t.datetime "oauth_expires_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "major"
   end
 
 end
