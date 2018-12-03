5403ff35b2339a394f06c7c0ecca7e6f4c32ffaf
Brian Stone <bstone26@uncc.edu>
2018-12-03 17:40:29 +0000

    destroy function now redirects to the page where is is called

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -40,7 +40,7 @@ class PostsController < ApplicationController
         @post = Post.find(params[:id])
         @post.destroy
         
-        redirect_to posts_path
+        redirect_back fallback_location: posts_path
     end
     
 end
