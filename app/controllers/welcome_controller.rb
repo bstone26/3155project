e9af33480d58b50c89228fcb539baf4ca0671d6c
Brian Stone <bstone26@uncc.edu>
2018-12-04 02:14:34 +0000

    Add the users major posts to the homepage

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,8 +1,8 @@
 class WelcomeController < ApplicationController
   def index
     if current_user
-      # @posts = Post.where(author_id: current_user.uid)
-      @posts = Post.where(category: current_user.major)
+      @posts = Post.where(author_id: current_user.uid)
+      @posts2 = Post.where(category: current_user.major)
     end
   end
 end
