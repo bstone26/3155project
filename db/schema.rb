562e11231c56c217b89e45714d63c6f85490036c
Brian Stone <bstone26@uncc.edu>
2018-12-04 03:10:48 +0000

    added like and dislike attributes to post

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_02_160642) do
+ActiveRecord::Schema.define(version: 2018_12_04_031000) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 2018_12_02_160642) do
     t.datetime "updated_at", null: false
     t.string "author"
     t.string "author_id"
+    t.integer "dislike"
+    t.integer "like"
   end
 
   create_table "replies", force: :cascade do |t|
